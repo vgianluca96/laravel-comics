84e82600c5b171c0994c90537b7c7369a8f55611
vgianluca96 <gianluca.vallese@hotmail.com>
2023-10-26 14:26:30 +0200

    creato layout prova
    creata home.blade che estende app.blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+<div class="container py-4">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        titolo
+                    </h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        titolo
+                    </h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        titolo
+                    </h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        titolo
+                    </h5>
+                </div>
+            </div>
+        </div>
+
+        @stop
